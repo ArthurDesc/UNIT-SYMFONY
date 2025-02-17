23a8281dbbcaab3d4bf636294149e10fc8777943
ArthurDesc <arthur.descourvieres@gmail.com>
2025-02-17 19:35:50 +0100

    Add initial frontend setup with Webpack, Stimulus, and basic styles

---
+++ app/assets/app.js
@@ -0,0 +1,9 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap'; 
